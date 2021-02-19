b558e92d2a57e259f818039d0c8ce1d6a2629ef7
DazaiLux <pauxcode@gmail.com>
2021-02-18 19:45:56 -0600

    Updates

---
+++ precache-manifest.618c847f02211d3ec2930d7cd053ffb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450af21472a34bde47426f39b682359b",
+    "url": "/React-App-Paises/index.html"
+  },
+  {
+    "revision": "c013ddbd911ef9506318",
+    "url": "/React-App-Paises/static/css/main.d9cb2149.chunk.css"
+  },
+  {
+    "revision": "75e33eefc1d7ed165434",
+    "url": "/React-App-Paises/static/js/2.02d370b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-App-Paises/static/js/2.02d370b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c013ddbd911ef9506318",
+    "url": "/React-App-Paises/static/js/main.01f85302.chunk.js"
+  },
+  {
+    "revision": "93409fa2bcb8d55fd557",
+    "url": "/React-App-Paises/static/js/runtime-main.fbe213c9.js"
+  }
+]);
